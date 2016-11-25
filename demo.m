a4cdee1a9ba6fa247745377bf8f1760e4d92f909
Sudeep Fadadu <fadadusudeep93@gmail.com>
2016-11-25 18:32:28 -0500

    updated demo file

--- demo.m
+++ demo.m
@@ -5,10 +5,14 @@ load 'camera.mat'
 imagefiles = dir('test_images/*.tif');      
 nfiles = length(imagefiles);    % Number of files found
 images = zeros(64,64,1,nfiles);
-for ii=1:nfiles
+for ii=1:nfiles-1
    currentfilename = imagefiles(ii).name;
    currentimage = imread(['test_images/' currentfilename]);
-   im = rgb2gray(currentimage);
+   im1 = rgb2gray(currentimage);
+   currentfilename = imagefiles(ii+1).name;
+   currentimage = imread(['test_images/' currentfilename]);
+   im2 = rgb2gray(currentimage);
+   
    %% Get Fundamental 
    
    %% Get Dense Correnspondences 
