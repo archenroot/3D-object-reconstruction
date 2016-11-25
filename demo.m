5503b2f262df56d229ff43ff0804c52792920abe
Sudeep Fadadu <fadadusudeep93@gmail.com>
2016-11-25 18:31:06 -0500

    added demo file

--- demo.m
+++ demo.m
@@ -1,2 +1,22 @@
 %% Demo script 
 
+load 'camera.mat'
+
+imagefiles = dir('test_images/*.tif');      
+nfiles = length(imagefiles);    % Number of files found
+images = zeros(64,64,1,nfiles);
+for ii=1:nfiles
+   currentfilename = imagefiles(ii).name;
+   currentimage = imread(['test_images/' currentfilename]);
+   im = rgb2gray(currentimage);
+   %% Get Fundamental 
+   
+   %% Get Dense Correnspondences 
+   
+   %% Get Subpixel and dense disparity
+   
+   %% Get Triangulation
+   
+   %% BA
+   
+end
